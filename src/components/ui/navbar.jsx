7005e4de52d76302d51c4a7052056fa275234789
Jared Elliott <bootcamp@jaredclt.com>
2024-02-22 22:19:55 -0500

    Updated class to className

--- src/components/ui/navbar.jsx
+++ src/components/ui/navbar.jsx
@@ -1,20 +1,21 @@
 export default function Nav({ links }) {
     return (
-      <nav class="navbar navbar-expand-md bg-primary" data-bs-theme="dark">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">JAE</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-md bg-primary" data-bs-theme="dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">JAE</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarColor01">
-            <ul class="navbar-nav me-auto">
-              {links.map((link) => <li className="nav-item">{link}</li>)}
+          <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav me-auto">
+              {links.map((link, index) => <li key={index} className="nav-item">{link}</li>)}
             </ul>
             <ul className="navbar-nav ms-md-auto">
-              <li class="nav-item">
-                  <a target="_blank" rel="noopener" class="nav-link" href="https://github.com/cltjared">
-                      <i class="bi bi-github"></i>
-                      <span class="d-md-none ms-2">GitHub</span></a>
+              <li className="nav-item">
+                  <a target="_blank" rel="noopener" className="nav-link" href="https://github.com/cltjared">
+                      <i className="bi bi-github"></i>
+                      <span className="d-md-none ms-2">GitHub</span>
+                  </a>
               </li>
           </ul>
           </div>
