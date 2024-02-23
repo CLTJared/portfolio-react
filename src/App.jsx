3a3d42761dfe32b10ee90c9d8cc39794dc77739f
Jared Elliott <bootcamp@jaredclt.com>
2024-02-22 20:26:35 -0500

    Simplified app.jsx file to necessary components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
-import { useState } from 'react'
+import { Outlet } from 'react-router-dom';
 import Navigation from './components/navigation'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <h1 class="navbar-brand">Jared Elliott</h1>
-        <Navigation />
-      </nav>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <br />
-        <p class="m-0 p-0">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        </p>
-      </div>
+      <Navigation />
+      <Outlet />
     </>
   )
 }
