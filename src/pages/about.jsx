e6f95d15f31a5dc34d591f25db0ce2aa476c8c85
Jared Elliott <bootcamp@jaredclt.com>
2024-02-24 17:26:11 -0500

    Updated about section

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,17 @@
 export default function AboutMe() {
   return (
     <div className="container pt-4" id="about-me">
+      <img className="float-end mx-2 mb-2" src="bio.jpeg" width="200" alt="biography photo - Jared" />
       <p className="lead text-light">Hello! My name is Jared Elliott and I am an aspiring Full Stack Developer!</p>
       <p>Let's get to know each other a bit more.</p>
-      <p>A little about me. I have a 14 year old daughter and a 5 year old Shiba Inu, named Nacho!</p>
+      <p>
+        A little about me. I have a 14 year old daughter and a 5 year old Shiba Inu, named Nacho!
+        I started my journey with web design & development back in 2004 with an Associates degree. At the time, I enjoyed working with the LAMP stack and
+        coming up with unique solutions to problems.
+      </p>
+      <p>
+        Career-wise, I decided to take my journey towards the IT Support side of things, but have found a renewed passion in wanting to learn new development and look into the career path I once pursued.
+      </p>
     </div>
   );
 }
