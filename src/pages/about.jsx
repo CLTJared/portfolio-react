31f54bacd6bbe2dc2e7a77e907221d2ceba7da4b
Jared Elliott <bootcamp@jaredclt.com>
2024-02-23 22:14:31 -0500

    Removed lorem ipsum & replaced with better text.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,10 +1,9 @@
 export default function AboutMe() {
   return (
     <div className="container pt-4" id="about-me">
-      <p className="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus, dolor quia? Velit nostrum deleniti repudiandae reiciendis rerum cum doloremque consectetur quod magnam voluptate pariatur at hic sint doloribus, suscipit tenetur?</p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro corporis nemo neque, at excepturi veniam autem perspiciatis, veritatis eius sed velit suscipit quasi! Tempore obcaecati quis quae, ipsam fugit dicta.
-      </p>
+      <p className="lead text-light">Hello! My name is Jared Elliott and I am an aspiring Full Stack Developer!</p>
+      <p>Let's get to know each other a bit more.</p>
+      <p>A little about me. I have a 14 year old daughter and a 5 year old Shiba Inu, named Nacho!</p>
     </div>
   );
 }
