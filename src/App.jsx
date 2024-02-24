8c66385bfbb09331c186f79765f3c58679ab3477
Jared Elliott <bootcamp@jaredclt.com>
2024-02-24 17:26:29 -0500

    Added footer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Navigation from './components/navigation'
+import Footer from './components/ui/footer'
 
 function App() {
   return (
     <>
       <Navigation />
       <Outlet />
+      <Footer />
     </>
   )
 }
---
+++ src/components/ui/footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+      <footer className="footer mt-5 fixed-bottom bg-dark">
+        <div className="container-fluid text-center">
+            <span className="text-muted">Made using Vite + React. JAE ©2024</span>
+        </div>
+      </footer>
+    );
+  }
