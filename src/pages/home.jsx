bdeba83ae93b73d311d6e1abe808058c8401f698
Jared Elliott <bootcamp@jaredclt.com>
2024-02-23 22:14:47 -0500

    Removed placeholder text & updated.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,8 @@ export default function HomePage() {
 
   return (
     <div className="container pt-4" id="home-page">
-        <p>The home page</p>
+        <p className="lead text-light">Thank you for taking the time to look at my portfolio of work.</p>
+        <p>I'm excited for us to get to know each other a bit more. Please take some time and look at my various projects and resume!</p>
     </div>
   );
 }
